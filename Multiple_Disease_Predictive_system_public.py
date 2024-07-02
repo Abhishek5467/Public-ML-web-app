5ce1b6f6d370f177688bac0ff53a1c43c5bc0866
Abhishek5467 <149815850+Abhishek5467@users.noreply.github.com>
2024-07-02 09:46:28 +0530

    Update Multiple_Disease_Predictive_system_public.py

--- Multiple_Disease_Predictive_system_public.py
+++ Multiple_Disease_Predictive_system_public.py
@@ -245,7 +245,7 @@ else:
     
     # creating button for prediction
     if(st.button("Breast Cancer Prediction Result")):
-        Breast_cancer_prediction = Breast_cancer_model.predict(np.asarray([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]).reshape(1,-1))
+        Breast_cancer_prediction = Breast_cancer_model.predict(np.asarray([mean_radius,mean_texture,mean_perimeter,mean_area,mean_smoothness,mean_compactness,mean_concavity,mean_concave_points,mean_symmetry,mean_fractal_dimension,radius_error,texture_error,perimeter_error,area_error,smoothness_error,compactness_error,concavity_error,concave_points_error,symmetry_error,fractal_dimension_error,worst_radius,worst_texture,worst_perimeter,worst_area,worst_smoothness,worst_compactness,worst_concavity,worst_concave_points,worst_symmetry,worst_fractal_dimension]).reshape(1,-1))
         
         if (Breast_cancer_prediction[0] == 0):
           Breast_cancer_diagnosis = 'The tumor is Malignant'
